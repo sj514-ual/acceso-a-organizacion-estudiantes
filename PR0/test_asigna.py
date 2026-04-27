8e5a75e5a5f0489e8a41aebbce2b53a55d5dee2b
sj514-ual <sj514@inlumine.ual.es>
2026-04-27 03:36:44 +0000

    tarea 7 test ok

--- PR0/test_asigna.py
+++ PR0/test_asigna.py
@@ -1,4 +1,10 @@
 import PR0.asigna as asigna
+import PR1.edadPR as programa  # <-- Esta línea es vital
+
 def test_valores():
     assert asigna.a == 1
     assert asigna.b == 2
+
+def test_variable_edad():
+    # El assert tiene que estar "dentro" de la función (con espacio)
+    assert hasattr(programa, 'edad')
---
+++ PR1/edadPR.py
@@ -0,0 +1,6 @@
+# Preguntamos el año al usuario y lo convertimos a número entero
+nacimiento = int(input("¿En qué año naciste?"))
+# Calculamos la edad que tendrá al final de 2026
+edad = 2026 - nacimiento
+# Mostramos el resultado en  pantalla
+print(edad)
